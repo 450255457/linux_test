96041bd1ccd7d6c6eff578812dd16028ec82fb09
450255457 <450255457@qq.com>
2015-08-13 18:20:54 +0800

    1820

--- jsoncpp_test.cpp
+++ jsoncpp_test.cpp
@@ -1,11 +1,28 @@
+﻿//g++ -g -o jsoncpp_test jsoncpp_test.cpp /usr/lib/libjson_linux-gcc-4.8_libmt.a
+
 #include <json/json.h>
 #include <string>
 #include <iostream>
 
 using namespace std;
 
+int ParseJsonFromString()
+{
+	const char* str = "{\"uploadid\": \"UP000000\",\"code\": 100,\"msg\": \"\",\"files\": \"\"}";
+
+	Json::Reader reader;
+	Json::Value root;
+	if (reader.parse(str, root))  // reader将Json字符串解析到root，root将包含Json里所有子元素  
+	{
+		std::string upload_id = root["uploadid"].asString();  // 访问节点，upload_id = "UP000000"  
+		int code = root["code"].asInt();    // 访问节点，code = 100  
+	}
+	return 0;
+}
+
 int main()
 {
+	ParseJsonFromString();
 	string test = "{\"id\":1,\"name\":\"kurama\"}";
 	Json::Reader reader;
 	Json::Value value;
