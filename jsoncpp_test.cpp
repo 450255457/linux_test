820135f362080ddfef6d2b97dfe187fd63a17ee5
450255457 <450255457@qq.com>
2015-08-14 14:33:50 +0800

    1433

--- jsoncpp_test.cpp
+++ jsoncpp_test.cpp
@@ -22,8 +22,33 @@ int ParseJsonFromString()
 	return 0;
 }
 
+void readJson() {
+	using namespace std;
+	std::string strValue = "{\"name\":\"json\",\"array\":[{\"cpp\":\"jsoncpp\"},{\"java\":\"jsoninjava\"},{\"php\":\"support\"}]}";
+
+	Json::Reader reader;
+	Json::Value value;
+
+	if (reader.parse(strValue, value))
+	{
+		std::string out = value["name"].asString();
+		std::cout << out << std::endl;
+		const Json::Value arrayObj = value["array"];
+		for (unsigned int i = 0; i < arrayObj.size(); i++)
+		{
+			if (!arrayObj[i].isMember("cpp"))
+				continue;
+			out = arrayObj[i]["cpp"].asString();
+			std::cout << out;
+			if (i != (arrayObj.size() - 1))
+				std::cout << std::endl;
+		}
+	}
+}
+
 int main()
 {
+	readJson();
 	ParseJsonFromString();
 	string test = "{\"id\":1,\"name\":\"kurama\"}";
 	Json::Reader reader;
