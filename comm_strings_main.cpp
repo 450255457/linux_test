13ed81b58b31933cf01259651b49cd019ebcbcc6
450255457 <450255457@qq.com>
2015-09-18 14:43:45 +0800

    09181443

--- comm_strings_main.cpp
+++ comm_strings_main.cpp
@@ -1,7 +1,7 @@
 ﻿/*****************************************
 > File Name : comm_strings_main.cpp
 > Description : some public function
-	g++ -g -o comm_strings_main comm_strings_main.cpp comm_strings.h
+	g++ -g -o comm_strings_main comm_strings_main.cpp comm_strings.h comm_strings.cpp
 > Author : linden
 > Date : 2015-08-07
 *******************************************/
@@ -12,9 +12,16 @@ using namespace std;
 
 int main()
 {
-	std::string str = "	1 2	3	";
+	std::string str = "	1 a	b	I Miss You";
 	printf("str = %s\n", str.c_str());
 	Trim(str);
 	printf("str = %s\n", str.c_str());
+	ToUpper(str);
+	printf("str = %s\n", str.c_str());
+	ToLower(str);
+	printf("str = %s\n", str.c_str());
+	char cstr[] = "what is the hell...";
+	string sstr = Format(cstr);
+	printf("sstr = %s\n", str.c_str());
 	return 0;
 }
