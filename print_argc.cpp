307d6a9a3e303244e3011f7fc5709b0beb178acc
450255457 <450255457@qq.com>
2016-01-22 11:35:35 +0800

    01221135

---
+++ print_argc.cpp
@@ -0,0 +1,17 @@
+/*****************************************
+> File Name : print_argc.cpp
+> Description : mainº¯ÊýÊ¾Àý´úÂë
+	gcc -g -o printArgc print_argc.cpp
+> Author : linden
+> Date : 2016-01-22
+*******************************************/
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+	for (i = 0; argv[i] != NULL; i++)
+	{
+		printf("argv[%d] = %s\n", i, argv[i]);
+	}
+	exit(0);
+}
