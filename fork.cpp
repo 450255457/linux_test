19a580b02b505bb83b6031c5b6babdabfcfa15c7
450255457 <450255457@qq.com>
2016-01-25 11:34:08 +0800

    01251134

--- fork.cpp
+++ fork.cpp
@@ -17,16 +17,17 @@ void pre_exit(int status)
 {
 	if (WIFEXITED(status))
 	{
-		printf("normal termination,exit status = %d\n", WEXITSTATUS(stauts));
+		printf("normal termination,exit status = %d\n", WEXITSTATUS(status));
 	}
 	else if (WIFSIGNALED(status))
 	{
 		printf("abnormal termination,signal number = %d%s\n", WTERMSIG(status),
-		#ifdef WCOREDUMP
-					WCOREDUMP(status) ? " (core fire generated) " : "");
-		#else
-					"");
-		#endif
+#ifdef WCOREDUMP
+			WCOREDUMP(status) ? " (core fire generated) " : "");
+#else
+			"");
+#endif
+	}
 	else if (WIFSTOPPED(status))
 	{
 		printf("child stopped,signal number = %d\n",WSTOPSIG(status));
