f0d77159d95b69bcafc64762f4e885bbed796805
450255457 <450255457@qq.com>
2016-01-27 17:10:02 +0800

    01271709

--- operator_overloading.cpp
+++ operator_overloading.cpp
@@ -13,6 +13,7 @@ struct Complex {
 	Complex( double r, double i ) : re(r), im(i)/*构造函数初始值列表*/ {}
 	Complex operator+( Complex &other );
 	friend istream &operator>>(istream &in, Complex &other);
+	friend ostream &operator<<(ostream &out, Complex &other);
 	void Display( ) {   cout << re << ", " << im << endl; }
 private:
 	double re, im;
@@ -29,9 +30,17 @@ istream &operator>>(istream &in, Complex &other)
 	in >> other.re >> other.im;
 	return in;
 }
+
+//重载输出运算符
+ostream &operator<<(ostream &out, Complex &other)
+{
+	cout << other.re << ", " << other.im;
+	return out;
+}
+
 int main() {
-	Complex a = Complex(1.2, 3.4);
-	Complex b = Complex(5.6, 7.8);
+	Complex a = Complex(0.0, 0.0);
+	Complex b = Complex(0.0, 0.0);
 	Complex c = Complex(0.0, 0.0);
 
 	cin >> a >> b;
@@ -40,5 +49,6 @@ int main() {
 	   当执行c = a + b 语句时，编译器检测到"+"号左边（"+"号具有左结合性）是一个 Complex 对象，就会调用运算符重载函数，该语句会被转换为： a.operator+(b)
    */
 	c = a + b;
-	c.Display();
+	//c.Display();
+	cout << c << endl;
 }
