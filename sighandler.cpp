fab8fc366db2b369f9569f25ac451eef657394d9
450255457 <450255457@qq.com>
2016-02-24 16:36:25 +0800

    02241636

--- sighandler.cpp
+++ sighandler.cpp
@@ -1,6 +1,6 @@
 ﻿/*****************************************
 > File Name : sighandler.cpp
-> Description : 信号处理程序
+> Description : 信号处理僵尸进程
 	g++ -g -o sighandler sighandler.cpp
 > Author : linden
 > Date : 2016-02-23
@@ -10,34 +10,23 @@
 
 using namespace std;
 
-static void sig_usr(int signo)	/* argument is signal number */
+int main(int argc, char *argv[])
 {
-	if (signo == SIGUSR1)
+	pid_t pid = fork();
+	if (0 > pid)
 	{
-		cout << "received SIGUSR1." << endl;
-	}
-	else if (signo == SIGUSR2)
+		perror("fork error:");
+		exit(-1);
+	} 
+	else if (0 == pid)
 	{
-		cout << "received SIGUSR2." << endl;
+		cout << "\nIm in child process:" << getpid() << endl;
+		exit(0);
 	}
 	else
 	{
-		cout << "received signal " << signo << endl;
-	}
-}
-int main(int argc, char *argv[])
-{
-	if (signal(SIGUSR1, sig_usr) == SIG_ERR)
-	{
-		cout << "can't catch SIGUSR1" << endl;
-	}
-	else if (signal(SIGUSR2, sig_usr) == SIG_ERR)
-	{
-		cout << "can't catch SIGUSR2" << endl;
-	}
-	for (;;)
-	{
-		pause();
+		cout << "\nIm in parent process." << endl;
+		sleep(600);
 	}
 	return 0;
 }
